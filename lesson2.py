430a2ae37bf8be88035788919f4c1f7b8e8c14d1
TetianaOvch <tanyshkaovch@gmail.com>
2022-10-24 21:37:28 +0300

    ex. lesson2

---
+++ lesson2.py
@@ -0,0 +1,12 @@
+# print(1 + 1)
+result = 1 + 3
+result = 3 - 2
+value_1 = 12
+value_2 = 5
+# result = value_1 * value_2
+# result = value_1 / value_2
+# result = value_1 // value_2
+# result = value_1 % value_2
+result = value_1 ** value_2
+
+print(result)
